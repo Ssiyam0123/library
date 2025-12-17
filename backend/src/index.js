427d3130026fa60f3ff15e6e4272914b7c9f40e1
Ssiyam0123 <ssiyam563@gmail.com>
2025-12-17 23:33:25 +0600

    push

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,17 @@ import express from "express"
 import "dotenv/config"
 import authRoute from "./routes/authRoutes.js";
 import { connectDb } from "./lib/db.js";
+import cors from 'cors'
+import cookieParser from "cookie-parser"
 
 const app = express();
 
+<<<<<<< Updated upstream
 const PORT = process.env.PORT;
+=======
+app.use(express.json());
+app.use(cors());
+>>>>>>> Stashed changes
 
 app.use("/api/auth",authRoute);
 
